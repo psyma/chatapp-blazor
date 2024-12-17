ec083a059f937e78d226cf30214962fc3874b0de
psyma <rickbengimeda@gmail.com>
2024-12-17 13:23:09 +0800

    Home page chat template

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,20 @@
-import "flowbite"
+import "flowbite"  
+import { initFlowbite } from "flowbite"
+import Bowser from "bowser"
 
-const user = document.getElementById("user");
-user.addEventListener("click", (e) => {
-    const sidebarBtn = document.getElementById("default-sidebar-button")
-    console.log(sidebarBtn.click())
-})
+window.initializeFlowbite = () => {
+    initFlowbite();
+}
+
+window.getUserAgentPlatformType = () => {
+    const browser = Bowser.getParser(window.navigator.userAgent);
+    return browser.getPlatform().type
+}
+
+window.onSidebarUserClick = async () => {
+    const sidebarBtn = document.getElementById("default-sidebar-btn") 
+    if (getUserAgentPlatformType() === "mobile") {
+        sidebarBtn.click()
+    }
+}
+
