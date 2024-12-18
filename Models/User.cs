64f7bc811d75a13a3d3d9149226c6f4aff246eb3
psyma <rickbengimeda@gmail.com>
2024-12-18 12:13:54 +0800

    Register template

--- Models/User.cs
+++ Models/User.cs
@@ -5,4 +5,5 @@ namespace chatapp_blazor.Models;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class User : IdentityUser
 {
+    public required string Name { get; set; }
 }
